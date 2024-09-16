a3bb39ea0ee494cf95ac343e9381c7b50d912640
Ryan Zhang (RIT Student) <rlz2910@rit.edu>
2024-09-16 09:43:14 -0400

    Started HW2

---
+++ Homeworks/HW_StatsAnalysis_Zhang/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Ryan Zhang
+ * HW 2 - Stats Analysis
+ * https://docs.google.com/document/d/13uxd-298WyJnL3v0W3L5OD_eGMS3xIth-ASy47Wy42s/edit
+ */
+namespace HW_StatsAnalysis_Zhang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Activity 1
+            // Player 1 Variables
+            string namePlayer1;
+            int gamesWonPlayer1;
+            int gamesLostPlayer1;
+            int gamesTotalPlayer1;
+            double hoursPlayedPlayer1;
+            double avgHoursPerGamePlayer1;
+            double winRatePlayer1;
+
+            // Player 2 Variables
+            string namePlayer2;
+            int gamesWonPlayer2;
+            int gamesLostPlayer2;
+            int gamesTotalPlayer2;
+            double hoursPlayedPlayer2;
+            double avgHoursPerGamePlayer2;
+            double winRatePlayer2;
+
+            // Formatted strings for Activity 2
+            string namePrompt = "Enter the name for Player {0}: ";
+            string gamesPlayedPrompt = "Enter the number of games {0} {1}: ";
+            string hoursPlayedPrompt = "Enter the total time played by {0} in hours: ";
+
+            // Formatted string for Activity 3
+            // TODO
+
+            // Error Flags
+            bool statValidPlayer1;
+            bool statValidPlayer2;
+
+            // Activity 2
+            Console.WriteLine("========= STATS ANALYZER =========\n");
+
+            // Get Player 1 inputs
+
+            // Player 1 Error Check
+
+            // Get Player 2 input
+
+            // Player 2 Error Check
+
+
+            // Activity 3
+            // Calculations
+
+            // Print Summary
+
+        }
+    }
+}
