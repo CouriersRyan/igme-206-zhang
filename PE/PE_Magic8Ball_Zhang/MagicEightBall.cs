c32c9c653e8af17bcc2d78556de0b9cf5044eb99
Ryan Zhang <rlz2910@rit.edu>
2024-10-01 16:23:37 -0400

    Added Magic 8 Ball PE

---
+++ PE/PE_Magic8Ball_Zhang/MagicEightBall.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Ryan Zhang
+ * PE - Magic 8 Ball
+ * https://docs.google.com/document/d/13OBHKxRLnRKNiYijs119CSW9Y7kW77oRCd1uovYR-Hs/edit?usp=sharing
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_Magic8Ball_Zhang
+{
+    /// <summary>
+    /// Class for Magic 8 Ball, gives the owner a response when the ball is shaken.
+    /// </summary>
+    internal class MagicEightBall
+    {
+        // name of the person holding the ball
+        private string owner;
+        // number of times the ball has been shaken
+        private int timesShaken;
+        // list of potential responses the ball can tell the user
+        private string[] responses;
+       
+        private Random random;
+
+        /// <summary>
+        /// Constructs a magic 8 ball with the owner as an input/
+        /// </summary>
+        /// <param name="owner"></param>
+        public MagicEightBall(string owner)
+        {
+            this.owner = owner;
+            timesShaken = 0;
+            responses = new string[5] {"Definitely.", "Not likely.", "Never.", "Cannot predict right now.", "Ask again later."};
+            random = new Random();
+        }
+
+        /// <summary>
+        /// Shakes the Magic 8 Ball and returns a random response. Increments the number of times shaken by one.
+        /// </summary>
+        /// <returns></returns>
+        public string ShakeBall()
+        {
+            timesShaken++;
+            return responses[random.Next(0, 5)];
+        }
+
+        /// <summary>
+        /// Returns a string stating how many times the ball has been shaken.
+        /// </summary>
+        /// <returns></returns>
+        public string Report()
+        {
+            switch (timesShaken)
+            {
+                case 0:
+                    return String.Format("{0} has not shaken the ball yet.", owner);
+
+                case 1:
+                case 2:
+                case 3:
+                    return String.Format("{0} has shaken the ball {1} times.", owner, timesShaken);
+
+                case >= 4:
+                    return String.Format("{0} has shaken the ball {1} times. That's a lot of questions!", owner, timesShaken);
+
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ PE/PE_Magic8Ball_Zhang/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Ryan Zhang
+ * PE - Magic 8 Ball
+ * https://docs.google.com/document/d/13OBHKxRLnRKNiYijs119CSW9Y7kW77oRCd1uovYR-Hs/edit?usp=sharing
+ */
+namespace PE_Magic8Ball_Zhang
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Runs the a MAgic 8 Ball simulator in console using the MagicEightBall class.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string input;
+            MagicEightBall magicEightBall;
+            bool isRunning = true; // true until the user wants to 'quit'
+
+            // Introduction and prompt for name.
+            Console.WriteLine("Welcome to Magic 8 Ball Simulator!");
+            Console.Write("  > Who owns this ball?  ");
+            input = Console.ReadLine().Trim();
+            Console.WriteLine();
+
+            // Make instance of MagicEightBall using user input.
+            magicEightBall = new MagicEightBall(input);
+
+            // Loops for user inputs to use the Magic 8 Ball.
+            while (isRunning)
+            {
+                // Ask for prompt
+                Console.WriteLine("What would you like to do?");
+                Console.Write("You can ‘shake’ the ball, get a ‘report’, or ‘quit’:  ");
+                input = Console.ReadLine().Trim().ToLower();
+
+                // shake
+                // ask for a question and get a response
+                if (input.Equals("shake"))
+                {
+                    Console.Write( "  > What is your question? ");
+                    Console.ReadLine(); // don't actually need to use the input
+                    Console.WriteLine("  > Magic 8 Ball says: {0}", magicEightBall.ShakeBall());
+                }
+                // report
+                // print how many times the ball has been shaken
+                else if (input.Equals("report"))
+                {
+                    Console.WriteLine("  > " + magicEightBall.Report());
+                }
+                // quit
+                // set loop variable to false
+                else if (input.Equals("quit"))
+                {
+                    isRunning = false;
+                    Console.WriteLine("  > Goodbye!");
+                } 
+                else 
+                { 
+                    Console.WriteLine("  > I do not recognize that response.");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
