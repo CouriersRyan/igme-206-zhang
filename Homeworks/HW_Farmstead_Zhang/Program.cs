adbcc65b28edb1e83377ab636ada765ac154bc24
Ryan Zhang (RIT Student) <rlz2910@rit.edu>
2024-10-08 17:34:34 -0400

    Added HW 5 Farmstead

---
+++ Homeworks/HW_Farmstead_Zhang/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Ryan Zhang 
+ * HW 5 - Farmstead
+ * https://docs.google.com/document/d/1xnF9pZIhLC-PW3OAOktW15VzMtAktTZWnHEsuQSDBpQ/edit?usp=sharing
+ */
+namespace HW_Farmstead_Zhang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
